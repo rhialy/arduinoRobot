c332d8843961fd92e81003f7172bf7fd3aaab536
Daniel Stehlik <stehlik@creunity.de>
2015-11-30 23:40:15 +0100

    Newnew

--- main.ino
+++ main.ino
@@ -1,108 +1,123 @@
+// --- Libraries ---------
+#include <NewPing.h>
+
+// --- Variables for sensors ---------
+
+#define  TRIGGER_PIN  11
+#define  ECHO_PIN     10
+#define MAX_DISTANCE 200
+
+int DistanceIn;
+int DistanceCm;
+int Spause = 100;
+int measuringRuntime = 300;
+bool isObstacle = false;
+
+// --- Variables for motor shield ---------
+
 int leftmotorForward = 8; // pin 8 --- left motor (+) green wire
-
 int leftmotorBackward = 11; // pin 11 --- left motor (-) black wire
-
 int leftmotorspeed = 9; // pin 9 --- left motor speed signal
-
 int rightmotorForward = 12; // pin 12 --- right motor (+) green wire
-
 int rightmotorBackward = 13; // pin 13 --- right motor (-) black
-
 int rightmotorspeed = 10; // pin 10 --- right motor speed signal
 
-//--- Speeds and Timers
-
-int pause = 2000; //Long delay time between steps
-
-int runtime = 5000; // How long Runtime actions will last
-
+int Mpause = 2000; //Long delay time between steps
+int moroRuntime = 1000; // How long motor actions last
 int slow = 230; // slow speed (of 255 max)
-
 int fast = 255; // fast speed (of 255 max)
 
 //------------------------------------------------------
 
-void setup() //---6 Pins being used are outputs--- 
-{
+void setup() { //---6 Pins being used are outputs--- 
 
 pinMode(leftmotorForward, OUTPUT);
-
 pinMode(leftmotorBackward, OUTPUT);
-
 pinMode(leftmotorspeed, OUTPUT);
-
 pinMode(rightmotorForward, OUTPUT);
-
 pinMode(rightmotorBackward, OUTPUT);
-
 pinMode(rightmotorspeed, OUTPUT);
-
+  
 }
 
 // --- Main Program Loop -----------------------------
 
-void loop()
-
-{
-
+void loop() {
+  
 goForward();
-
-delay(runtime);
-
-Stop();
-
-delay(pause);
-
-goBackward();
-
-delay(runtime);
+delay(motorRuntime);
+measuring();
+delay(measuringRuntime);
+if(isObstacle == true) {
+  turnLeft();
+  delay(motorRuntime);
+  measuring();
+  delay(measuringRuntime);
+}
 
 }
 
-//----- "Sub-rutine" Voids called by the main loop
+// --- Methods for ultrasonic sound sensor  ---------
 
-void goForward()
+void measuring() {
 
-{
+  delay(Spause);
+  DistanceCm = sonar.ping_cm();
+  delay(Spause);
+  if(DistanceCm < 40) {
+    isObstacle = true;
+  } else {
+    isObstacle = false;
+  }
+  }
+}
+
+
+// --- Methods for motor shield ---------
+
+void turnLeft() {
+  analogWrite(leftmotorspeed,Fast);
+  digitalWrite(leftmotorBackward,LOW);
+  digitalWrite(leftmotorForward,HIGH);
+  digitalWrite(rightmotorBackward,LOW);
+  digitalWrite(rightmotorForward,LOW);
+}
+
+void turnLeft() {
+  analogWrite(leftmotorspeed,Fast);
+  digitalWrite(leftmotorBackward,LOW);
+  digitalWrite(leftmotorForward,LOW);
+  digitalWrite(rightmotorBackward,LOW);
+  digitalWrite(rightmotorForward,HIGH);
+}
+
+
+
+void goForward() {
 
 analogWrite(leftmotorspeed,Fast); //Enable left motor by setting speed
-
 analogWrite(rightmotorspeed,Fast); //Enable left motor by setting speed
-
 digitalWrite(leftmotorBackward,LOW); // Drives LOW outputs down first to avoid damage
-
 digitalWrite(rightmotorBackward,LOW);
-
 digitalWrite(leftmotorForward,HIGH);
-
 digitalWrite(rightmotorForward,HIGH);
 
 }
 
-void goBackward()
-
-{
+void goBackward() {
 
 analogWrite(leftmotorspeed,Slow);
-
 analogWrite(rightmotorspeed,Slow);
-
 digitalWrite(leftmotorForward,LOW);
-
 digitalWrite(rightmotorForward,LOW);
-
 digitalWrite(leftmotorBackward,HIGH);
-
 digitalWrite(rightmotorBackward,HIGH);
 
 }
 
-void Stop() // Sets speed pins to LOW disabling both motors
-
-{
+void Stop() { // Sets speed pins to LOW disabling both motors
 
 digitalWrite(leftmotorspeed,LOW);
-
 digitalWrite(rightmotorspeed,LOW);
 
 }
